486da1a5f18e99277ec9cbe7407e6afb8cc69d72
umangparekh <umangparekh.in@gmail.com>
2025-09-07 14:58:48 +0200

    CTCI-Chapter1

--- java-xperiments/src/main/java/org/example/ZeroMatrix.java
+++ java-xperiments/src/main/java/org/example/ZeroMatrix.java
@@ -1,2 +1,55 @@
-package org.example;public class ZeroMatrix {
+package org.example;
+
+public class ZeroMatrix {
+
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+
+        // Step 1: Mark rows and columns
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        // Step 2: Zero out rows
+        for (int i = 0; i < m; i++) {
+            if (rows[i]) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 3: Zero out columns
+        for (int j = 0; j < n; j++) {
+            if (cols[j]) {
+                for (int i = 0; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,  2,  3, 4},
+                {5,  0,  7, 8},
+                {9,  10,  11, 12},
+                {13, 14, 15, 16}
+        };
+        setZeroes(matrix);
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
 }
